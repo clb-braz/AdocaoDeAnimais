4130c9695e8a91de2fac4df92799f0d21bebf62b
clb-braz <clb.braz@protonmail.com>
2025-06-03 15:08:32 -0300

    Create SistemaAdocaoApplication.java

---
+++ src/main/java/com/adocao/sistema/SistemaAdocaoApplication.java
@@ -0,0 +1,11 @@
+package com.adocao.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAdocaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaAdocaoApplication.class, args);
+    }
+}
